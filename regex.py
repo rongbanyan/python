62b900e1ba688dc6424fcdc39b8d6be6842e4400
rongbanyan <rongliner@sina.cn>
2018-08-06 13:54:18 +0800

    正则表达式的使用
    写一个函数，它使用正则表达式，确保传入的口令字符串是强口令。强口令的定义是：长度不少于8 个字符，同时包含大写和小写字符，至少有一位数字。你可能需要用多个正则表达式来测试该字符串，以保证它的强度。
    1.写一个函数，它使用正则表达式，确保传入的口令字符串是强口令。强口令的定义是：长度不少于8 个字符，同时包含大写和小写字符，至少有一位数字。你可能需要用多个正则表达式来测试该字符串，以保证它的强度。
    
    2. 写一个函数，它接受一个字符串，做的事情和strip()字符串方法一样。如果只传入了要去除的字符串，没有其他参数，那么就从该字符串首尾去除空白字符。否
    则，函数第二个参数指定的字符将从该字符串中去除。

--- regex.py
+++ regex.py
@@ -1,7 +1,10 @@
 # coding:utf-8
 # 强口令
 """
-写一个函数，它使用正则表达式，确保传入的口令字符串是强口令。强口令的定义是：长度不少于8 个字符，同时包含大写和小写字符，至少有一位数字。你可能需要用多个正则表达式来测试该字符串，以保证它的强度。
+1.写一个函数，它使用正则表达式，确保传入的口令字符串是强口令。强口令的定义是：长度不少于8 个字符，同时包含大写和小写字符，至少有一位数字。你可能需要用多个正则表达式来测试该字符串，以保证它的强度。
+
+2. 写一个函数，它接受一个字符串，做的事情和strip()字符串方法一样。如果只传入了要去除的字符串，没有其他参数，那么就从该字符串首尾去除空白字符。否
+则，函数第二个参数指定的字符将从该字符串中去除。
 """
 
 import re
@@ -21,3 +24,16 @@ def checkPassword(password):
         return False
 
 print (checkPassword('12geaesA'))
+
+def likestrip(strings, chars=None):
+    if chars is None:
+        reg = re.compile('^ *| *$')
+    else:
+        reg = re.compile('^[' + chars + ']*|[' + chars + ']*$')
+    return reg.sub('', strings)
+
+print(likestrip('    123456   '))
+print(likestrip('      123456'))
+print(likestrip('123456    123456'))
+print(likestrip('123456    123456', '1'))
+print(likestrip('123456    123456', '123'))
